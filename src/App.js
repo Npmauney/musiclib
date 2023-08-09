4d6c4ac5d10238b053403adf0d54a43a96f4aa6c
Npmauney <nathanielmauney@gmail.com>
2023-08-09 18:32:31 -0400

    starting code along

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import {useState, useEffect} from 'react'
 import Gallery from './components/Gallery';
 import SearchBar from './components/SearchBar';
+
 function App() {
   let [message, setMessage] = useState('Seach for music')
   let [search, setSearch] = useState('')
   let [data, setData] = useState([])
   const API_URL = 'https://itunes.apple.com/search?term='
+
   useEffect(()=>{
-    if(search){
-    const fetchData = async () => {
-      document.title = `${search} music`
-      const response = await fetch(API_URL + search)
-      const resData = await response.json()
-      if(resData.results.length > 0){
+    if (search) {
+      const fetchData = async () => {
+        document.title = `${search} music`
+        const response = await fetch(API_URL + search)
+        const resData = await response.json()
+
+      if (resData.results.length > 0) {
         setData(resData.results)
-      }else{
+      } else {
         setMessage('Not Found')
       }
     }
     fetchData()
     }
   }, [search])
+
   const handleSearch = (e, term) => {
     e.preventDefault();
     setSearch(term)
