d0bc636cc9dd02a3122a80aa20fc95a5e71a8190
Npmauney <nathanielmauney@gmail.com>
2023-08-12 09:57:59 -0400

    created Router links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import {useState, useEffect} from 'react'
+import {useState, useEffect, Fragment} from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery';
 import SearchBar from './components/SearchBar';
+import AlbumView from './components/AlbumView';
+import ArtistView from './components/ArtistView';
 
 function App() {
   let [message, setMessage] = useState('Seach for music')
@@ -30,11 +33,22 @@ function App() {
     setSearch(term)
   }
   return (
-    <div className="App">
-      <SearchBar handleSearch={handleSearch}/>
-      {message}
-      <Gallery data={data}/>
+    <div>
+    {message}
+        <Router>
+            <Routes>
+                <Route path="/" element={
+                    <Fragment>
+                        <SearchBar handleSearch = {handleSearch}/>
+                        <Gallery data={data} />
+                    </Fragment>
+                } />
+                <Route path="/album/:id" element={<AlbumView />} />
+                <Route path="/artist/:id" element={<ArtistView />} />
+            </Routes>
+        </Router>
     </div>
-  );
+)
+
 }
 export default App;
