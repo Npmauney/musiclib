b12b8f5edab65f67ab343b170a121b524b8eb925
Npmauney <nathanielmauney@gmail.com>
2023-08-13 15:00:12 -0400

    implemented Spinner

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,7 +2,9 @@ import React from 'react'
 import GalleryItem from './GalleryItem'
 
 function Gallery(props) {
-    const display = props.data.map((item, i)=> {
+    const data = props.data.result.read()
+
+    const display = data.map((item, i)=> {
         return (
             <GalleryItem item={item} key={i}/>
         )
